f9679a67a909e0e6810e158eae6d0709e8bf88b7
zchelalo <eduardosaavedra687@gmail.com>
2026-03-21 19:15:01 -0700

    feature: add account ports layer

---
+++ internal/modules/account/ports/account_store.go
@@ -0,0 +1,17 @@
+package ports
+
+import (
+	"context"
+	"time"
+
+	"github.com/zchelalo/expense-control-back/internal/modules/account/domain"
+)
+
+type AccountRepository interface {
+	Create(ctx context.Context, account domain.Account) error
+	ByID(ctx context.Context, accountID domain.AccountID) (domain.Account, error)
+	ListByUserID(ctx context.Context, userID domain.UserID, createdAt *time.Time, accountID *domain.AccountID, limit int) ([]domain.Account, error)
+	UpdateName(ctx context.Context, accountID domain.AccountID, name domain.Name, now time.Time) error
+	UpdateBalance(ctx context.Context, accountID domain.AccountID, balance domain.Balance, now time.Time) error
+	Delete(ctx context.Context, accountID domain.AccountID, now time.Time) error
+}
